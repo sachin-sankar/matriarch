79a38fe66dada8d52e3f10c3d070a00a3880605d
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-09-01 22:53:14 +0530

    feat: Basic implementation in QML

---
+++ shell.nix
@@ -0,0 +1,38 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+(pkgs.buildFHSEnv {
+  name = "pyside-fhs-env";
+  targetPkgs =
+    pkgs:
+    (with pkgs; [
+      # Compression & Core C Libraries
+      zstd # Fixes libzstd.so.1
+      zlib
+      glib
+      openssl
+      libxml2
+      libxslt
+      krb5
+
+      # GUI, Fonts & Display
+      libGL
+      fontconfig
+      freetype
+      libxkbcommon
+      dbus
+      wayland
+
+      # X11 Libraries
+      libX11
+      libXext
+      libXrender
+      libXi
+      libXcursor
+      libXrandr
+      libxcb
+    ]);
+
+  runScript = "bash";
+}).env
