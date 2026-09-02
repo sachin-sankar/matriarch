9c0c9115e35aab43ab6218be9a5b1273a6ad2b97
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-09-03 02:27:28 +0530

    fix: remove duplicate ErrorResponse fields

--- mockwork/server.py
+++ mockwork/server.py
@@ -111,6 +111,9 @@ class ErrorResponse(BaseModel):
     """Error response model."""
 
     detail: str
+    """Error response model."""
+
+    detail: str
 
 
 def _get_app(req: Request) -> Optional[App]:
