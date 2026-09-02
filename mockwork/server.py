59205475f64db8de94a6ffdf16583c160fc67fac
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-09-03 02:27:22 +0530

    fix: clean up ErrorResponse model

--- mockwork/server.py
+++ mockwork/server.py
@@ -108,6 +108,9 @@ class ErrorResponse(BaseModel):
     """Error response model."""
 
     detail: str
+    """Error response model."""
+
+    detail: str
 
 
 def _get_app(req: Request) -> Optional[App]:
